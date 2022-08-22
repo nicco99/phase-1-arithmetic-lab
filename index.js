94262181541a7ac0b6704d01db3d5e44cd55ea5a
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-22 12:17:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+// Write your code her
 
-// Write your code here
+let num1=31, num2=2;
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random()*10);
+// const random = (random>0)?Math.floor(Math.random()*10):false;
+
+// const random = (number)=>{
+//     if(number>0){
+//         return number;
+//     } else {
+//         return "number is a zero"
+//     }
+// }
+// console.log(random(number))
+
+//mod test
+let  num3 = 25;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+
+let max = (1,1,2,3,15,20)
+Math.max = (max)
